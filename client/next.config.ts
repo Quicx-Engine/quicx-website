7f77e26dca38500d9320c9449a9e65eaa43a3101
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-05-28 11:26:06 +0300

    Disable svg indexing

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*.svg",
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
